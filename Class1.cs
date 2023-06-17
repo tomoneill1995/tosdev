b250bfe0b1a80c76c776c117cb2b55c29f963b97
tomoneill1995 <tomoneill.1995@gmail.com>
2023-06-17 23:07:32 +1000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using HarmonyLib;
+
+
+namespace TosDev
+{
+    public class MyMod : MelonMod
+    {
+        public virtual void OnInitializeMelon()
+        {
+            new Harmony("MyMod").PatchAll();
+            ((MelonBase)this).LoggerInstance.Msg("QoL modifications loaded");
+        }
+    }
+}
---
+++ devpatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Game.Simulation;
+using System.Reflection;
+
+
+namespace TosDev
+{
+    [HarmonyPatch(typeof(GameSimulation), "get_userLevel")]
+    internal class Devpatch
+    {
+
+
+        
+        [HarmonyPrefix]
+        internal static bool Prefix()
+        {
+
+            Type exampleType = typeof(GameSimulation);
+            MethodInfo set_userLevel = exampleType.GetMethod("set_userLevel");
+            set_userLevel.Invoke(null, new object[] { 3 });
+
+            MethodInfo get_userLevel = exampleType.GetMethod("get_userLevel");
+            get_userLevel.Invoke(null, new object[] {});
+            return true;
+        }
+    }
+}
